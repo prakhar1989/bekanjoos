d2312c3fe3957d560132e31c28c50c39a288ed95
gaurangsadekar <gaurang.sadekar@gmail.com>
2015-12-05 18:06:43 -0500

    made s3 module

--- api-server/s3.js
+++ api-server/s3.js
@@ -2,46 +2,48 @@ var AWS = require('aws-sdk');
 var wget = require('wget-improved');
 var fs = require('fs');
 
+exports.storeInS3 = function(image_url, callback) {
+  var bucketName = 'bekanjoos-imgs';
+  var src = image_url;
+  var url_paths = src.split("/");
+  var filename = url_paths[url_paths.length - 1];
+  var filename = filename.split(';')[0];
+  var output = 'temp/' + filename;
+  var temp_filepath = output;
 
-var bucketName = 'bekanjoos-imgs';
-var src = 'http://img6a.flixcart.com/image/sunglass/r/s/b/fa-1102-c1-farenheit-m-400x400-imae37gyr5q8fpwg.jpeg';
-//var output = 'temp/'
-var url_paths = src.split("/");
-var filename = url_paths[url_paths.length - 1];
-var ext = filename.split(".")[1];
-var output = 'temp/' + filename;
-var temp_filepath = output;
-var download = wget.download(src, output, {});
-download.on('error', function(err) {
-    console.log(err);
-});
-
-download.on('end', function (output) {
-  fs.readFile(temp_filepath, function(err, data) {
-    if (err) {
+  var s3PublicUrl = '';
+  var download = wget.download(src, output, {});
+  download.on('error', function(err) {
       console.log(err);
-    }
-    else {
-      var s3 = new AWS.S3({params : {Bucket: bucketName}});
-      var data = {
-        Key: filename,
-        Body: data,
-        ContentType: 'image/'+ext,
-        ACL: 'public-read'
-      };
-      s3.putObject(data, function(err, data){
-          if (!err) {
-            console.log('succesfully uploaded the image!');
-            var publicUrl = 'https://s3.amazonaws.com/' + bucketName + '/' + filename;
-            console.log(publicUrl);
-          }
-      });
-    }
   });
-  // deleting the temp file from system
-  fs.unlink(temp_filepath, function(err) {
-    if (!err) {
-      console.log('Temp image deleted');
-    }
+
+  download.on('end', function (output) {
+    fs.readFile(temp_filepath, function(err, data) {
+      if (err) {
+        console.log(err);
+      }
+      else {
+        var s3 = new AWS.S3({params : {Bucket: bucketName}});
+        var data = {
+          Key: filename,
+          Body: data,
+          ContentType: 'image/jpg',
+          ACL: 'public-read'
+        };
+        s3.putObject(data, function(err, data){
+            if (!err) {
+              console.log('succesfully uploaded the image!');
+              s3PublicUrl = 'https://s3.amazonaws.com/' + bucketName + '/' + filename;
+              callback(s3PublicUrl);
+            }
+        });
+      }
+    });
+    // deleting the temp file from system
+    fs.unlink(temp_filepath, function(err) {
+      if (!err) {
+        console.log('Temp image deleted');
+      }
+    });
   });
-});
+}
