e6fddf54d8578934b993abd6392129ac97ce310b
Prakhar Srivastav <prakhar1989@gmail.com>
2015-12-05 16:00:19 -0500

    Facebook welcome msg added

--- chrome-extension/extension/js/new-popup.js
+++ chrome-extension/extension/js/new-popup.js
@@ -1,33 +1,32 @@
-function completedLogin() {
-    chrome.tabs.query({active:true, currentWindow: true}, function(tabs) {
-        var tab = tabs[0];
-        var url = tab.url;
-    });
-
-    alert("you are now logged in");
-}
-
 document.addEventListener('DOMContentLoaded', function() {
     var loginButton = document.getElementById("login");
     var logoutButton = document.getElementById("logout");
     var expiry = new Date(parseInt(localStorage.expiryTime));
     var now = new Date();
 
+    // add the event listener
+    loginButton.addEventListener("click", function() {
+        var redirect_url = "https://www.facebook.com/connect/login_success.html";
+        var url = "https://www.facebook.com/dialog/oauth?client_id=1643610532594445&"
+                            + "redirect_uri=" + redirect_url
+                            + "&scope=email&response_type=token";
+        chrome.tabs.create({url: url, selected: true});
+    });
+
+
     if (localStorage.accessToken && now < expiry) {
-        var p = document.getElementById("pre");
-        p.innerText = "awesome you're logged in";
+        // logged in
+        document.getElementById("msg").innerText = "hello, " + localStorage.name;
         loginButton.style.visibility = "hidden";
-    } else {
-        console.log("here");
+    } else { // init login
         logoutButton.style.visibility = "hidden";
     }
 
-    loginButton.addEventListener("click", function() {
-        loginFacebook(completedLogin);
-    });
-
     logoutButton.addEventListener("click", function() {
         localStorage.accessToken = null;
+        localStorage.id = null;
+        localStorage.email = null;
+        localStorage.user = null;
         localStorage.expiryTime = null;
     });
 });
