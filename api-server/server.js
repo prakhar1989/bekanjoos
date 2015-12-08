897757e7bdda1dd1e1a082232b7f6b9c0a565dc3
gaurangsadekar <gaurang.sadekar@gmail.com>
2015-12-08 18:02:42 -0500

    REST API server

--- api-server/server.js
+++ api-server/server.js
@@ -1,43 +1,23 @@
-// server.js
-
-// BASE SETUP
-// =============================================================================
-
-// call the packages we need
 var express = require('express');
 var app = express();
-var mongodb = require('mongodb');
-var MongoClient = mongodb.MongoClient;
-//var db;
+
+var bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+var http = require('http');
+var AWS = require('aws-sdk');
+var s3Writer = require('./s3.js');
 
 
-// Connection URL. This is where your mongodb server is running.
-var url = 'mongodb://localhost:27017/pricetell';
+// Connection URL for RDS instance
+var url = 'RDS URL';
 
-var port = process.env.PORT || 8080;        // set our port
+var port = process.env.PORT || 9000;        // set our port
 
 // ROUTES FOR OUR API
 // =============================================================================
 var router = express.Router();              // get an instance of the express Router
-
-
-
-// Use connect method to connect to the Server
-  /*MongoClient.connect(url, function (err, db) {
-    if (err) {
-      console.log('Unable to connect to the mongoDB server. Error:', err);
-    } else {
-      //HURRAY!! We are connected. :)
-      console.log('Connection established to', url);
-      //this.db = db;
-
-      // do some work here with the database.
-
-      //Close connection
-      //db.close();
-  }
-
-});*/
 
 // middleware to use for all requests
 router.use(function(req, res, next) {
@@ -49,7 +29,7 @@ router.use(function(req, res, next) {
 
 // test route to make sure everything is working (accessed at GET http://localhost:8080/api)
 router.get('/', function(req, res) {
-    res.json({ message: 'hooray! welcome to our api!' });
+    res.json({message: 'hooray! welcome to our api!'});
 });
 
 
@@ -57,41 +37,55 @@ router.route('/products')
 
     // get all the products assigned to a user (accessed at GET http://localhost:8080/api/:user_id)
     .get(function(req, res) {
-
-      res.json({ message: 'Get Request Called' });
-        MongoClient.connect(url, function (err, db) {
-          if (err) {
-            console.log('Unable to connect to the mongoDB server. Error:', err);
-          }
-          else{
-      var cursor = db.collection('users').find( );
-      cursor.each(function(err, doc) {
-      //  assert.equal(err, null);
-        if (doc != null) {
-         console.log(doc);
-        } else {
-         //callback();
-        }
-     });
-   }
-   });
+   //
+  //     res.json({ message: 'Get Request Called' });
+  //       MongoClient.connect(url, function (err, db) {
+  //         if (err) {
+  //           console.log('Unable to connect to the mongoDB server. Error:', err);
+  //         }
+  //         else{
+  //     var cursor = db.collection('users').find( );
+  //     cursor.each(function(err, doc) {
+  //     //  assert.equal(err, null);
+  //       if (doc != null) {
+  //        console.log(doc);
+  //       } else {
+  //        //callback();
+  //       }
+  //    });
+  //  }
+  //  });
 
     })
 
 router.route('/product')
     // add a product for the user (accessed at POST http://localhost:8080/api/:user_id/product)
     .post(function(req, res) {
+      var title = req.body.title;
+      var url = req.body.url;
+      var site = req.body.site;
+      var price = req.body.price;
+      var product_id = req.body.product_id;
+      var image_url = req.body.image_url;
+      console.log();
+      s3Writer.storeInS3(image_url, function(publicUrl) {
+        var s3PublicUrl = publicUrl;
+        console.log(s3PublicUrl);
 
-      res.json({ message: 'Post Request Called' });
-
+      });
+      res.json({message: 'Got a request'});
     })
 
     // update the product for the user (accessed at POST http://localhost:8080/api/:user_id/product)
     .delete(function(req, res) {
-
-      res.json({ message: 'Delete Request Called' });
+      res.json({ status: 'product added', id: 10 });
 
     });
+
+router.route('/newprice')
+  .post(function(req, res) {
+
+  })
 
 
 
@@ -104,4 +98,4 @@ app.use('/api', router);
 // START THE SERVER
 // =============================================================================
 app.listen(port);
-console.log('Magic happens on port ' + port);
+console.log('Listening on 9000');
