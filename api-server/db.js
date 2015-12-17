facc0111cf63479b2ccfd189d2e7ac8a2cac196a
viralshahrf <viralshahrf@gmail.com>
2015-12-16 22:23:42 -0500

    Database APIs and Testing

--- api-server/db.js
+++ api-server/db.js
@@ -15,25 +15,22 @@ var con = mysql.createConnection({
 //findProductUsers("Amazon","9773585360");
 //findUserDetails(6467090862);
 //disconnect();
+//findUserProducts(6467090862);
 
-// functions we need to service
-// get all products that a user is tracking
-//
-
-// not needed, everything works without it also
-exports.establishConnection =  function (){
+exports.establishConnection =  function (callback){
     con.connect(function(err){
         if (err) {
           console.log('Connection to the database could not be established');
+          callback(false);
         } else {
           console.log('Connection established');
+          callback(true);
         }
     });
 };
 
 exports.registerUser = function(facebookid, emailid) {
     var user = {fbid: facebookid, email: emailid};
-    // check if user already exists, may need to take some action in this case.
     con.query('SELECT fbid FROM user WHERE fbid = ?', facebookid, function(err, result) {
         if (!err) {
           if (result.length != 0)
@@ -41,7 +38,6 @@ exports.registerUser = function(facebookid, emailid) {
           else {
             con.query('INSERT INTO user SET ?', user, function(err,res){
                 if (err) {
-                  // put some better error logging
                   console.log('User could not be registered');
                 } else {
                   console.log('User registered');
@@ -56,66 +52,60 @@ exports.registerUser = function(facebookid, emailid) {
 };
 
 exports.doesProductExist = function (productSite, productId, callback) {
-  con.query('SELECT * FROM product WHERE site="' + productSite + '" AND pid="' + productId + '"', function(err, result) {
-    if (!err) {
-      if (result[0] !== undefined) {
-        callback(true);
-      } else
+    con.query('SELECT * FROM product WHERE site="' + productSite + '" AND pid="' + productId + '"', function(err, result) {
+      if (err) {
+        console.log('Product is not valid');
         callback(false);
-    }
-    else {
-      console.log(err);
-    }
-  })
+      } else {
+        if (result[0] != undefined) {
+          console.log('Product exists');
+          callback(true);
+        } else {
+          console.log('Product does not exist');
+          callback(false);
+        }
+      }
+    })
 };
 
 exports.addProduct = function (productSite, productId, productTitle, productImage, productPrice, callback) {
-  var product = {site: productSite, pid: productId, title: productTitle, image: productImage, price: productPrice};
-  con.query('INSERT INTO product SET?', product, function(err,res){
-    if (!err) {
-      callback(true);
-    }
-    else
-      callback(false);
-  });
+    var product = {site: productSite, pid: productId, title: productTitle, image: productImage, price: productPrice};
+    con.query('INSERT INTO product SET?', product, function(err,res){
+      if (err) {
+        console.log('Failed to add product');
+        callback(false);
+      } else {
+        console.log('Product successfully added');
+        callback(true);
+      }
+    });
 };
 
 exports.registerUserProduct = function (facebookid, productSite, productId, callback){
     var userProduct = {fbid: facebookid, site: productSite, pid: productId};
     con.query('INSERT INTO userProducts SET?', userProduct, function(err,res){
       if (err) {
-         // user already tracking the product
-         callback('exists');
+        console.log('User is already tracking the product');
+        callback('exists');
       } else {
-        // new product added for user
+        console.log('Product added for the user');
         callback('added');
       }
     });
 };
 
-exports.unTrackProduct = function (facebookid, productSite, productId, callback) {
-  con.query("DELETE FROM userProducts WHERE fbid = " + facebookid + " AND site = '" + productSite + "' AND pid = " +  productId, function(err, res) {
-    if (!err) {
-      callback(true);
-    }
-    else {
-      console.log(err);
-      callback(false);
-    }
-  })
-};
-
-function updateProductPrice(productSite, productId, updatedPrice){
+exports.updateProductPrice = function (productSite, productId, updatedPrice, callback){
     con.query('SELECT price FROM product WHERE site="' + productSite + '" AND pid="' + productId + '"', function(err,res){
       if (err) {
         console.log('Product price could not be queried');
-        return 0;
+        callback(-1);
       } else {
         var productPrice = res[0].price;
         if (updatedPrice != productPrice) {
           con.query('UPDATE product SET price=' + updatedPrice + ' WHERE site="' + productSite + '" AND pid="' + productId + '"', function(err,res){
             if (err) {
               console.log('Product price could not be updated');
+              callback(-1);
             } else {
               console.log('Product price has been updated');
             }
@@ -123,53 +113,63 @@ function updateProductPrice(productSite, productId, updatedPrice){
         }
         var updateTime = null;
         con.query('SELECT CURRENT_TIMESTAMP', function(err,res){
-          if (!err) {
+          if (err) {
+            console.log('Current time could not be polled');
+          } else {
             updateTime = res[0].CURRENT_TIMESTAMP;
             var priceUpdate = {site: productSite, pid: productId, time: updateTime, price: updatedPrice};
             con.query('INSERT INTO priceHistory SET?', priceUpdate, function(err,res){
               if (err) {
-                console.log(err);
                 console.log('Price history failed to update');
+                callback(-1);
               } else {
                 console.log('Price history updated');
               }
             });
           }
         });
-        return productPrice - updatedPrice;
+        callback(updatedPrice - productPrice);
       }
     });
 };
 
-function findProductUsers(productSite, productId){
+exports.findProductUsers = function (productSite, productId, callback){
     con.query('SELECT email FROM user, userProducts WHERE user.fbid = userProducts.fbid AND site="' + productSite + '" AND pid="' + productId + '"', function(err,res){
       if (err) {
         console.log('Retrieval of product users failed');
-        return null;
+        callback(null);
       } else {
         console.log('Retrieved product users');
-        console.log(res);
-        return res;
+        callback(res);
       }
     });
 };
 
-function findUserDetails(facebookid){
+exports.findUserDetails = function (facebookid, callback){
     con.query('SELECT * FROM user WHERE fbid="' + facebookid + '"', function(err,res){
       if (err) {
         console.log('User not registered');
-        return null;
+        callback(null);
       } else {
         console.log('Retrieved user details');
-        console.log(res);
-        return res;
+        callback(res);
       }
     });
 };
 
+exports.findUserProducts = function (facebookid, callback){
+    con.query('SELECT site, pid, title, image, price from product WHERE (site,pid) IN (SELECT site, pid FROM userProducts WHERE fbid ="' + facebookid + '")', function(err,res){
+      if (err) {
+        console.log('Products not found for the user');
+        callback(null);
+      } else {
+        console.log('Retrieved products for the user');
+        callback(res);
+      }
+    });
+};
 
-
-function disconnect(){
+exports.disconnect = function (){
     con.end(function(err){
         if (err ){
           console.log('Connection could not be gracefully terminated');
---
+++ api-server/test.js
@@ -0,0 +1,16 @@
+var db = require('./db.js');
+
+function test(testvar) {
+    console.log('database api works');
+};
+
+//db.establishConnection(test);
+//db.registerUser(6467090862,"viralshahrf@gmail.com");
+//db.addProduct("Amazon","9773585360","LordOfTheRings","https://i.ytimg.com/vi/0B8C2MkkEqI/maxresdefault.jpg",30, test);
+//db.registerUserProduct(6467090862,"Amazon","9773585360", test);
+//db.updateProductPrice("Amazon","9773585360",35, test);
+//db.findProductUsers("Amazon","9773585360", test);
+//db.findUserDetails(6467090862, test);
+//db.disconnect();
+//db.findUserProducts(6467090862, test);
+//db.doesProductExist("Amazon", "9773585360", test);
